7bc64d8e5b5f5f76c7e3c1fc4eba7189ed5a5942
eli-sesa <60906377+eli-sesa@users.noreply.github.com>
2020-08-07 11:28:47 -0700

    Add files via upload

--- nhtsa_data_analysis.py
+++ nhtsa_data_analysis.py
@@ -43,134 +43,12 @@ def cfc(data, cfc_class, T):
     return Y2
 
 
-
 def finite_difference(x, timeStep):  
     dx = np.zeros(len(x))
     for i in range(len(x)-1):
         dx[i] = (x[i+1] - x[i]) / timeStep
     dx[-1] = dx[-2]
     return dx
-
-
-
-
-timeStep = 1/20000
-
-cfcClass = 20
-
-filenames = ['AX.csv', 'AY.csv']
-fig, axs = plt.subplots(4, 2, sharey='row', sharex=True)
-
-fig.suptitle("CFC %d"%cfcClass)
-
-plotcol = 0
-
-dataOut = pd.DataFrame()
-# Linear Accelerations
-for filename in filenames:
-    
-    data = pd.read_csv(filename, usecols=range(1,11))
-    time = np.arange(-.05, .3+timeStep, timeStep)
-    
-    for headers in data.columns:  
-        
-        # Filter and Integrate
-        accFilt = cfc(data[headers], cfcClass, timeStep)
-        vel = scipy.integrate.cumtrapz(9.81 * accFilt, time, initial=0)
-        pos = scipy.integrate.cumtrapz(vel, time, initial=0)
-        
-        # compute raw position without all of the extra steps.  Messy, but saves variables
-        posraw = scipy.integrate.cumtrapz(
-            scipy.integrate.cumtrapz(
-                9.81 * data[headers],
-                time, 
-                initial=0)
-            ,time,
-            initial=0)
-            
-        poserror = 1000 * (posraw - pos) # position error due to filtering in mm
-
-        # Plot everything 
-        axs[0, plotcol].set_title(filename.replace('.csv',''))
-        
-        axs[0, plotcol].plot(time, accFilt)
-        axs[1, plotcol].plot(time, vel)
-        axs[2, plotcol].plot(time, pos)
-        axs[3, plotcol].plot(time, poserror)
-        
-        
-        # Compute things for  Data table
-        
-        # imax = np.argmax(poserror) # index of the max position error
-        # print('Final Position Error = %f mm' %poserror[-1])
-        # print("Max Position error of %f mm at %f seconds" %(poserror[imax], time[imax]))
-        # print('\n\n')
-        
-        # scipy.signal.findpeaks
-        
-        # Output filtered data
-        dataOut[headers] = accFilt
-        
-        
-    # At the end, increase the column to plot into
-    plotcol +=1
-
-
-    dataOut.to_csv(filename[0:2]+'Filtered'+str(cfcClass)+'.csv')
-
-# With the figure, add ylabels to the left column        
-ylabels = ('Acc (g)', 'Vel (m/s)', 'Pos (m)', ' $\Delta$ Pos (mm)')
-for i in range(4):
-    axs[i, 0].set_ylabel(ylabels[i])
-
-plt.legend(data.columns)
-        
-#%%
-# Yaw Data
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-#     ############################# Old WOrk 
-#     wRaw = np.array(data[headers])
-#     np.fft.fft(wRaw)
-    
-#     wFilt = cfc(wRaw, cfcClass, timeStep)
-#     alphaFilt = finite_difference(wFilt, timeStep)
-
-#     # plt.plot(time, alphaRaw)
-#     plt.plot(time, alphaFilt)
-    
-#     # wFilt = cfc(data[headers], cfcClass, timeStep)
-    
-    
-#     # alphaRaw = scipy.integrate.cumtrapz(wRaw, time, initial=0)
-#     # alphaFilt = scipy.integrate.cumtrapz(wFilt, time, initial=0)
-    
-     
-    
-#     # axs[0].plot(time, cfc(alphaRaw, cfcClass, timeStep))
-#     # axs[0].set_ylabel('Integrate Raw, then Filter')
-#     # axs[1].plot(time, alphaFilt)
-#     # axs[1].set_ylabel('Integrate Filtered')
-#     # axs[2].plot(time, wRaw)
-
-# plt.legend(data.columns)
-#%% 
-# Derivitive Sandbox
-# https://www.youtube.com/watch?v=y8SqkjoKV4k
-
-# wRaw = data['9586']
-# wFilt = cfc(wRaw, cfcClass, timeStep)
-dataOut = pd.DataFrame()
 
 
 def filt_dif(x, filterClass, timeStep):
@@ -194,68 +72,175 @@ def spectral_dif(x, timeStep):
     dx = np.real(np.fft.ifft(dxHat))
     return dx
 
+timeStep = 1/20000
+
+cfcClass = 20
+SAVE_CSV = False
+
+maxStroke = 2
+maxTime = .150
+Xmax = 2
+Ymax = .600
+
+filenames = ['AX.csv', 'AY.csv']
+
+fig, axs = plt.subplots(4, 2, sharey='row', sharex=True)
+fig.suptitle("CFC %d"%cfcClass)
+
+dataOut = pd.DataFrame()
+output = pd.DataFrame(
+    columns=(
+        'TestNo',
+        'Direction',
+        'MaxAccX', 
+        'MaxAccY', 
+        'XFinal',
+        'YFinal',
+        'TwhereX',
+        'TwhereY',
+        'MaxXError',
+        'FinalXError',
+        'MaxYError',
+        'FinalYError',
+        ))
+
+plotcol = 0
+# Linear Accelerations
+for filename in filenames:
+    
+    data = pd.read_csv(filename, usecols=range(1,11))
+    time = np.arange(-.05, .3+timeStep, timeStep)
+    
+    for headers in data.columns:  
+        
+        # Filter and Integrate
+        accFilt = cfc(data[headers], cfcClass, timeStep)
+        vel = scipy.integrate.cumtrapz(9.81 * accFilt, time, initial=0)
+        pos = scipy.integrate.cumtrapz(vel, time, initial=0)
+        
+        # compute raw position without all of the extra steps.  Messy, but saves variables
+        posraw = scipy.integrate.cumtrapz(
+            scipy.integrate.cumtrapz(
+                9.81 * data[headers],
+                time, 
+                initial=0)
+            ,time,
+            initial=0)
+            
+        posError = 1000 * (posraw - pos) # position error due to filtering in mm
+
+        # Plot everything 
+        axs[0, plotcol].set_title(filename.replace('.csv',''))
+        
+        axs[0, plotcol].plot(time, accFilt)
+        axs[1, plotcol].plot(time, vel)
+        axs[2, plotcol].plot(time, pos)
+        axs[3, plotcol].plot(time, posError)
+        
+        
+        # Compute things for  Data table
+    
+        # imax = np.argmax(posError) # index of the max position error
+        # print('Final Position Error = %f mm' %posError[-1])
+        # print("Max Position error of %f mm at %f seconds" %(posError[imax], time[imax]))
+        # print('\n\n')
+        
+        output['TestNo'] = headers
+        output['Direction'] = filename[1]
+        # This doesnt work but leaving here for readability while I figure it out 
+        # if filename[1] == 'X':
+        #     output['MaxAccX'].append(np.max(accFilt))
+        #     output['XFinal'] = time[(time - maxTime).argmin()]
+        #     output['TwhereX'] = time[(pos - Xmax).argmin()]
+        #     output['MaxXError'] = np.max(posError)
+        #     output['FinalXError'] = posError[-1]
+               
+        outputStats = np.array([headers,
+                                filename[1],
+                                np.max(accFilt),
+                                pos[np.abs(time - maxTime).argmin()], 
+                                time[np.abs(pos - Xmax).argmin()],
+                                np.max(posError),
+                                posError[-1]])
+        
+        print(outputStats)
+        
+        
+        if filename[1] == 'Y':
+            output['MaxAccY'] = np.max(accFilt)
+            output['YFinal'] = time[np.abs(time - maxTime).argmin()]
+            output['TwhereY'] = time[np.abs(pos - Xmax).argmin()]
+            output['MaxYError'] = np.max(posError)
+            output['FinalYError'] = posError[-1] 
+        
+        
+        # Output filtered data to csv
+        dataOut[headers] = accFilt       
+       # scipy.signal.findpeaks
+    # At the end, increase the column to plot into
+    plotcol +=1
+
+    if SAVE_CSV:
+        dataOut.to_csv(filename[0:2]+'Filtered'+str(cfcClass)+'.csv')
+
+# With the figure, add ylabels to the left column        
+ylabels = ('Acc (g)', 'Vel (m/s)', 'Pos (m)', ' $\Delta$ Pos (mm)')
+for i in range(4):
+    axs[i, 0].set_ylabel(ylabels[i])
+
+plt.legend(data.columns)
+axs[2, 0].hlines(2.0, time[0], .150, linestyle='dashed', zorder=1)
+axs[2, 0].vlines(.150, 0, 2.0, linestyle='dashed',  zorder=1)
+
+axs[2, 1].hlines(.6, time[0], .150,  linestyle='dashed', zorder=1)  
+axs[2, 1].vlines(.150, 0, .6, linestyle='dashed', zorder=1)      
+
+#%% 
+# Yaw Data
+
+# wRaw = data['9586']
+# wFilt = cfc(wRaw, cfcClass, timeStep)
+dataOut = pd.DataFrame()
+
+
 filename = 'WZ.csv'
 
 data = pd.read_csv(filename, usecols=range(1, 11))
 fig, axs = plt.subplots(4, 1, sharex=True)
-
+fig.suptitle('$\omega_z$')
 for headers in data.columns:
       wRaw = np.array(data[headers])
       wFilt = cfc(wRaw, cfcClass, timeStep)
       theta = scipy.integrate.cumtrapz(wFilt, time, initial=0)
       
-      axs[0].plot(time, filt_dif(wRaw, cfcClass, timeStep))
-      axs[0].set_ylabel('Acc: Filter then Diff')
+      thetaRaw = scipy.integrate.cumtrapz(wRaw, time, initial=0)
+      angleError = (thetaRaw - theta)
       
-      axs[1].plot(time, dif_filt(wRaw, cfcClass, timeStep))
-      axs[1].set_ylabel('Acc: Diff then Filter')
+      axs[0].plot(time, filt_dif(wRaw, cfcClass, timeStep))
+      axs[0].set_ylabel('$ \\alpha$ (deg/s^2)')
 
-      axs[2].plot(time, wFilt)
-      axs[2].set_ylabel('$\omega_z$ (deg/s)')
+      axs[1].plot(time, wFilt)
+      axs[1].set_ylabel('$\omega_z$ (deg/s)')
 
-      axs[3].plot(time, theta)
-      axs[3].set_ylabel('$\Theta$ (deg)')
+      axs[2].plot(time, theta)
+      axs[2].set_ylabel('$\Theta$ (deg)')
 
+      axs[3].plot(time, angleError)
+      axs[3].set_ylabel('Angle Error (deg)')
+      
+      
       dataOut[headers] = filt_dif(wRaw, cfcClass, timeStep)
 
 plt.legend(data.columns)
-    
-dataOut.to_csv(filename[0:2]+'Filtered'+str(cfcClass)+'.csv')
 
-# plt.plot(time, filt_dif(wRaw, cfcClass, timeStep),'.')
-# plt.plot(time, dif_filt(wRaw, cfcClass, timeStep),'.')
-# plt.plot(time[50:-50], spectral_dif(wFilt[50:-50], timeStep),'.')
-
-
-
+if SAVE_CSV:
+    dataOut.to_csv(filename[0:2]+'Filtered'+str(cfcClass)+'.csv')
 
 
 #%%
-######WORKSPACE#######
-L = wRaw.size * timeStep
-# Compute FD of filtered data
-alphaRaw = finite_difference(wRaw, timeStep)
-alphaFilt = finite_difference(wFilt, timeStep)
-
-# Compute Derivitive of data w FFT.  Let "hat" denote fft'd signal
-wRawHat = np.fft.fft(wFilt)
-omega = np.fft.fftfreq (wFilt.size, timeStep/(2*np.pi))
-alphaHat = omega * wRawHat * [1j] # Uses the fact that we know  fft(dx/dt) = i*omega*fft(x)
-alphaFFT = np.real(np.fft.ifft(alphaHat))
-
 plt.figure()
+plt.plot(filt_dif(wRaw, cfcClass, timeStep),'x')
+plt.plot(dif_filt(wRaw, cfcClass, timeStep),'o')
+plt.plot(spectral_dif(cfc(wRaw, cfcClass, timeStep), timeStep))
 
-plt.plot(time, alphaFilt,'.')
-plt.plot(time, cfc(alphaRaw, cfcClass, timeStep),'.')
-plt.plot(time, alphaFFT,'.')
-
-
-plt.legend(('Filter then FD','FD then Filter', 'Spectral Derivitive'))
-
-
-# Wrap all in functions if needed
-
-
-
-
-
+plt.legend(('filter then diff','diff then filter','spectral derivitave'))
